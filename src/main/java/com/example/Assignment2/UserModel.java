d5dd3a133d8c2add15f57a57f0187294a48bc41b
Nitin Dhemiwalm <dhemiwal.nitin@gmail.com>
2021-02-04 01:13:22 +0530

    lombok function added

--- src/main/java/com/example/Assignment2/UserModel.java
+++ src/main/java/com/example/Assignment2/UserModel.java
@@ -5,6 +5,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
 public class UserModel {
 
@@ -36,59 +42,59 @@ public class UserModel {
 //    public void setUserName(String userName)   { this.userName=userName; }
 
 
-    public Integer getId() {
-        return Id;
-    }
-
-    public void setId(Integer id) {
-        Id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getMobileNumber() {
-        return mobileNumber;
-    }
-
-    public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
-
-    public String getEmailID() {
-        return emailID;
-    }
-
-    public void setEmailID(String emailID) {
-        this.emailID = emailID;
-    }
-
-    public String getAddress1() {
-        return address1;
-    }
-
-    public void setAddress1(String address1) {
-        this.address1 = address1;
-    }
-
-    public String getAddress2() {
-        return address2;
-    }
-
-    public void setAddress2(String address2) {
-        this.address2 = address2;
-    }
+//    public Integer getId() {
+//        return Id;
+//    }
+//
+//    public void setId(Integer id) {
+//        Id = id;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public String getMobileNumber() {
+//        return mobileNumber;
+//    }
+//
+//    public void setMobileNumber(String mobileNumber) {
+//        this.mobileNumber = mobileNumber;
+//    }
+//
+//    public String getEmailID() {
+//        return emailID;
+//    }
+//
+//    public void setEmailID(String emailID) {
+//        this.emailID = emailID;
+//    }
+//
+//    public String getAddress1() {
+//        return address1;
+//    }
+//
+//    public void setAddress1(String address1) {
+//        this.address1 = address1;
+//    }
+//
+//    public String getAddress2() {
+//        return address2;
+//    }
+//
+//    public void setAddress2(String address2) {
+//        this.address2 = address2;
+//    }
 }
